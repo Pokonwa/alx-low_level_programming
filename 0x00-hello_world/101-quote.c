a34a078ebc952568a12741e31f1d95447b6cb638
Pokonwa <uzenduezinne@gmail.com>
2022-09-09 16:24:04 -0700

    Execute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,10 @@
  * Description: prints Dora's quote
  *
  * Return: Always 1 (Success)
-  
- * */
-
-int main(void)
+ *
+*/
+iint main(void)
 {
-      	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
