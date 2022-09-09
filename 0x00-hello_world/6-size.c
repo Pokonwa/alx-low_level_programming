ddb9a2cc8dfe20f33e71a5d5d22e3c49f8be614a
Pokonwa <uzenduezinne@gmail.com>
2022-09-09 08:45:42 -0700

    Size estimate

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of various types
+ *
+ * Desciption: prints the size of various types on the computer it is compiled
+ * and run on
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(int));
+	print("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", (int)sizeof(float));
+	return (0);
+}
