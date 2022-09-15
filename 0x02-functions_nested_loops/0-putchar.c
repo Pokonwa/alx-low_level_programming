51aa8c674451f11064bf2146cfc951ae5aed5198
Pokonwa <uzenduezinne@gmail.com>
2022-09-14 17:28:11 -0700

    Execute

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <main_h>
 /**
- * main - Entry
- * Return: returns on success
+ * main - Prints "_putchar" followed by a new line
+ * Return: Always 0.
  */
 int main(void)
 {
