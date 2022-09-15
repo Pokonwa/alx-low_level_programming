6d187800b2e82e9328ab360f4cd571bdfbb86b48
Pokonwa <uzenduezinne@gmail.com>
2022-09-15 10:07:06 -0700

    Reach it

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry, Prints 10x alphabets in lower case
+ * Description: It prints 10 times the alphabet followed by a new line
+ * Return: 0;
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+	}
+}
